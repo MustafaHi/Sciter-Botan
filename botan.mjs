4724d67e65e791e3378da6f392d0c3bfcd4d8c08
MustafaHi <matterik971@gmail.com>
2021-11-03 20:17:11 +0300

    JS 2.0+
    JavaScript API is modularized

--- botan.mjs
+++ botan.mjs
@@ -1,30 +1,33 @@
-//| Sciter-Botan 2.0
+//| Sciter-Botan 2.0+
 //| https://github.com/MustafaHi/Sciter-Botan
 
-if (!globalThis.Botan) 
-{
-    import { loadLibrary } from "@sciter";
-    globalThis.Botan = loadLibrary("sciter-botan");
-}
+import { loadLibrary } from "@sciter";
+
+const Botan = globalThis.Botan || loadLibrary("sciter-botan");
 
 
-Botan.password = function(method, data, hash = "") {
+export function password(method, data, hash = "") {
     return new Promise((resolve, reject) => {
         function CB(data, pass = true) { pass ? resolve(data) : reject(data); }
         Botan.passwordN(CB, method, data, hash);
     });
 }
 
-Botan.cipher = function(method, data, key, iv = "") {
+export function cipher(method, data, key, iv = "") {
     return new Promise((resolve, reject) => {
         function CB(data, pass = true) { pass ? resolve(data) : reject(data); }
         Botan.cipherN(CB, method, data, key, iv);
     });
 }
 
-Botan.decipher = function(method, data, key, iv) {
+export function decipher(method, data, key, iv) {
     return new Promise((resolve, reject) => {
         function CB(data, pass = true) { pass ? resolve(data) : reject(data); }
         Botan.decipherN(CB, method, data, key, iv);
     });
 }
+
+export function encode() { return Botan.encode(...arguments); }
+export function decode() { return Botan.decode(...arguments); }
+export function hash()   { return Botan.hash(...arguments); }
+export function iv()     { return Botan.iv(...arguments); }
